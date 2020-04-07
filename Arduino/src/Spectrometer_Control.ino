7003167731cdc8370459f860908bb899ae74073f
eduu97 <eduardencinas@gmail.com>
2020-04-07 18:57:30 +0200

    Code chenges

--- Arduino/src/Spectrometer_Control.ino
+++ Arduino/src/Spectrometer_Control.ino
@@ -33,6 +33,7 @@ void setup() {
   int read_time = 30;      // Amount of time that the analogRead() procedure takes (in microseconds) (different micros will have different times)
   int intTime = 300;
   int i;
+  long int numIntegrationBlocks = 1000;
 
 
   // Step 2: Send start pulse to signal start of integration/light collection
@@ -48,8 +49,7 @@ void setup() {
   delayMicroseconds(delay_time);
 
   // Step 3: Integration time -- sample for a period of time determined by the intTime parameter
-  int blockTime = delay_time;
-  long int numIntegrationBlocks = 1000;
+
   for (int i = 0; i < numIntegrationBlocks; i++) {
     digitalWrite(SPEC_CLK, LOW);
     delayMicroseconds(delay_time);
@@ -116,6 +116,7 @@ void print_data()
     Serial.print(',');
   }
   Serial.print("\n");
+  
 }
 
 void loop()
